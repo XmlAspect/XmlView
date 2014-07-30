2beeb557b4060bea2372cb74a3d61a33ab5e0335
sashafirsov <suns@firsov.net>
2014-07-29 21:27:53 -0700

    initial bookmarklet

---
+++ AsTable.xsl
@@ -0,0 +1,34 @@
+﻿<?xml version="1.0" encoding="UTF-8"?>
+
+<xsl:stylesheet version="1.0"
+xmlns:xsl="http://www.w3.org/1999/XSL/Transform">
+  <xsl:output
+  method="xml"
+  omit-xml-declaration="no"
+  standalone="yes"
+  indent="yes"
+  />
+  
+  <xsl:template match="/">
+	  Table
+      <xsl:apply-templates select="*"></xsl:apply-templates> 
+  </xsl:template>
+  <xsl:template match="*">
+    <xsl:apply-templates select="*" ></xsl:apply-templates>
+  </xsl:template>
+
+  <xsl:template match="country">
+	  <tr>
+		  <td>
+			  <xsl:value-of select="iso"/>
+		  </td>
+		  <td>
+			  <xsl:value-of select="name"/>
+		  </td>
+		  <td>
+			  <xsl:value-of select="population"/>
+		  </td>
+
+	  </tr>
+  </xsl:template>
+</xsl:stylesheet>
---
+++ RunXslt.js
@@ -0,0 +1,29 @@
+﻿(function()
+{   var xslUrl = "AsTable.xsl";
+    document.body.innerHTML = "<a href='"+xslUrl+"'> loading "+xslUrl+"</a>";
+    getXml( document.URL, function (xml)
+    {   getXml( document.URL, function (xsl, p, r)
+        {   p = new XSLTProcessor();
+            p.importStylesheet(xsl);
+            r = p.transformToFragment( xml, document );
+            document.body.replaceChild( r, document.body.firstChild );
+        });
+    });
+
+    function getXml(url, callback)
+    {   var xhr = new XMLHttpRequest();
+        xhr.url = url;
+        xhr.open("GET", url, false);
+        xhr.setRequestHeader( 'Origin', document.location.origin );
+//        xhr.overrideMimeType( 'text/xml' );
+        xhr.onreadystatechange = function()
+        {
+            if( 4 != xhr.readyState )
+                return;
+            if( xhr.responseXML )
+                return callback( xhr.responseXML );
+            return new DOMParser().parseFromString( xhr.responseText, "application/xml" );
+        }
+        xhr.send();
+    }
+})()
